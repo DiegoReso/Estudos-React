85ab498a6c9a4bf66fddccb3ef244363268d2eeb
Diego Reis <diegoreso@me.com>
2023-05-23 09:43:14 -0300

    create data validation register page

--- src/pages/RegisterUser.js
+++ src/pages/RegisterUser.js
@@ -1,28 +1,118 @@
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+
 
 const RegisterUser=()=>{
+
+
+const [form,setForm] = useState({
+  name:{
+    value: '',
+    error: false,
+    helperText: '',
+  },
+  job:{
+    value: '',
+    error: false,
+    helperText: '',
+  }
+})
+
+  
+
+
+  const handleInputChange = e =>{
+    const {name,value} = e.target
+    
+    setForm({
+      ...form,
+      [name]:{
+        value,
+      }
+    })
+  }
+
+
+  const handleRegisterButton =()=>{
+
+    let hasError = false
+    
+    const newFormState = {
+      ...form,
+    }
+
+
+    if(!form.name.value){
+      hasError = true
+
+      newFormState.name ={
+        value: form.name.value,
+        error:true,
+        helperText: 'Digite o seu nome',
+      }
+    }
+
+
+
+    if(!form.job.value){
+      hasError = true
+      
+      newFormState.job ={
+        value: form.job.value,
+        error:true,
+        helperText: 'Digite a sua função',
+      }
+    }
+
+
+    if(hasError){
+      return setForm(newFormState) 
+    }
+
+    axios.post('https://reqres.in/api/users',{
+      name: form.name.value,
+      job: form.job.value,
+    })
+    .then(resp =>console.log(resp))
+
+  }
+
+
   return(
     <>
       <div>
         <TextField
+          error={form.name.error}
+          helperText={form.name.helperText}
           sx={{mt: '40px'}}
-          id="outlined-basic"
           label="Nome"
-          variant="outlined" />
+          variant="outlined"
+          name='name'
+          value={form.name.value}
+          onChange={handleInputChange}
+          />
       </div>
 
       <div>
         <TextField
+        error={form.job.error}
         sx={{mt: '40px'}}
-        id="outlined-basic"
         label="Função"
-        variant="outlined" />
+        variant="outlined"
+        name='job'
+        helperText={form.job.helperText}
+        value={form.job.value}
+        onChange={handleInputChange}
+        />
       </div>
 
       <Button
         sx={{mt: '40px'}}
-        variant='contained'>
+        variant='contained'
+        onClick={handleRegisterButton}
+        >
           Registrar
       </Button> 
 
